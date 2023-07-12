3cb666c490c02bca6fc9ed78c1ea36c163bb511d
huige233 <huige1463@gmail.com>
2023-07-12 10:08:54 +0800

    should add tooltip

--- src/main/java/com/github/medium/ItemMedium.java
+++ src/main/java/com/github/medium/ItemMedium.java
@@ -27,6 +27,7 @@ public class ItemMedium extends Item implements ICurioItem {
 
     @Override
     public void appendHoverText(ItemStack p_41421_, @Nullable Level p_41422_, List<Component> p_41423_, TooltipFlag p_41424_) {
+        //p_41423.add("xxxxx.xxxx"); it example
     }
 
 
